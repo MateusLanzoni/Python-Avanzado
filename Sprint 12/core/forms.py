9be0b57dd5d98320f09e89b9eb56d7a773db3e03
Paulina0209 <paulina.medina@eia.edu.co>
2025-06-20 23:36:11 -0500

    modificación del readme y los forms para el formato fecha

--- Sprint 12/core/forms.py
+++ Sprint 12/core/forms.py
@@ -1,6 +1,7 @@
 # core/forms.py
 from django import forms
 from .models import Propietario, Mascota, Cita
+from django.forms import DateInput
 
 class PropietarioForm(forms.ModelForm):
     class Meta: model, fields = Propietario, "__all__"
@@ -9,4 +10,10 @@ class MascotaForm(forms.ModelForm):
     class Meta: model, fields = Mascota, "__all__"
 
 class CitaForm(forms.ModelForm):
-    class Meta: model, fields = Cita, "__all__"
+    fecha = forms.DateField(
+        input_formats=["%d/%m/%Y"],
+        widget=DateInput(format="%d/%m/%Y", attrs={"placeholder": "DD/MM/AAAA"})
+    )
+    class Meta:
+        model = Cita
+        fields = "__all__"
--- Sprint 12/veterinaria_web/settings.py
+++ Sprint 12/veterinaria_web/settings.py
@@ -111,6 +111,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS = [
+    "%d/%m/%Y",    # 21/06/2025
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
